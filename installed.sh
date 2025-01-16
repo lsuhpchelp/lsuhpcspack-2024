1f54645b19e8ce9b6c98a008bd7deec4c041e44d
Feng Chen <fchen14@lsu.edu>
2025-01-16 12:05:48 -0600

    updated the installation script

--- installed.sh
+++ installed.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 spack install gcc@9.5.0  % gcc@8.5.0
+spack load gcc@9.5.0
+spack compiler find
 spack install gcc@10.5.0 % gcc@9.5.0 arch=linux-rhel8-cascadelake
 spack install gcc@11.4.0 % gcc@9.5.0 arch=linux-rhel8-cascadelake
 spack install gcc@12.3.0 % gcc@9.5.0 arch=linux-rhel8-cascadelake
 spack install gcc@13.2.0 % gcc@9.5.0 arch=linux-rhel8-cascadelake
+spack load gcc@13.2.0
+spack compiler find
 spack install gromacs@2021.7 % intel ^gcc@10.5.0 ^intel-oneapi-mkl +cluster mpi_family=mpich arch=linux-rhel8-cascadelake 
 spack install gromacs@2021.7 +cuda % intel ^gcc@10.5.0 ^intel-oneapi-mkl +cluster mpi_family=mpich arch=linux-rhel8-cascadelake
 spack install intel-oneapi-mkl +cluster mpi_family=mpich arch=linux-rhel8-cascadelake
@@ -11,8 +15,11 @@ spack install petsc+fftw+valgrind % intel ^intel-oneapi-mkl+cluster mpi_family=m
 spack install nwchem +openmp +fftw3 %intel ^intel-oneapi-mkl+cluster mpi_family=mpich arch=linux-rhel8-cascadelake
 spack install parallel % intel arch=linux-rhel8-cascadelake
 spack install parallel-netcdf % intel arch=linux-rhel8-cascadelake
-spack install netcdf-fortran % intel arch=linux-rhel8-cascadelake
 spack install netcdf-c % intel arch=linux-rhel8-cascadelake
+spack install netcdf-fortran@4.6.1 % intel arch=linux-rhel8-cascadelake
 spack install netcdf-cxx % intel arch=linux-rhel8-cascadelake
 spack install quantum-espresso % intel arch=linux-rhel8-cascadelake
 spack install namd@3.0b7 % intel arch=linux-rhel8-cascadelake
+spack install vmd % intel arch=linux-rhel8-cascadelake
+spack install r@4.3.0 % gcc@13.2.0 arch=linux-rhel8-cascadelake
+spack install lammps % intel ^intel-oneapi-mkl +cluster mpi_family=mpich arch=linux-rhel8-cascadelake
